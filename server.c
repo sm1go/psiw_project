26c9188d8a9a54e5530a2be0816459722dd20bdc
Kacper <kacper.noweta@gmail.com>
2026-01-25 23:39:17 +0100

    build func added

--- server.c
+++ server.c
@@ -1,13 +1,23 @@
 #include "common.h"
 
+void sem_lock()
+
+void sem_unlock()
 
 
 
 
 
-
-void build(){
-    //budowa jednostek
+void build(int player_id, int unit_type, int count){
+    if (fork() == 0){
+        for (int i = 0; i < count; i++){
+            sleep(PRODUCTION_TIME[unit_type]);
+            sem_lock();
+            game->players[player_id].units[units_type]++;
+            sem_unlock;
+        }
+        exit(0);
+    }
 }
 
 
