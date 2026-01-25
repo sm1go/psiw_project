2d9276c372c6cdb2888df026950b82581962f732
Kacper <kacper.noweta@gmail.com>
2026-01-25 22:32:06 +0100

    draft

---
+++ server.c
@@ -0,0 +1,34 @@
+#include "common.h"
+
+
+
+
+
+
+
+void build(){
+    //budowa jednostek
+}
+
+
+
+
+void battle(){
+    //symulacja walki, logika walki
+}
+
+
+void attack(){
+
+}
+
+
+
+void updates
+
+
+void gold 
+
+void cleanup
+
+int main
