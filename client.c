eceb2496761167c08f95614ad12621b76d78221a
Kacper <kacper.noweta@gmail.com>
2026-01-25 22:37:28 +0100

    client draft

---
+++ client.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+
+
+void clear_screen(){
+
+}
+
+
+void print_dashboard(){
+ // jakis ekran startowy do gry, podstawowe informacje, zloto i punkty, info o jednostkach, opis komend
+}
+
+void receiver(){ //sluchanie z serwera
+
+}
+
+
+int main(){
+    // logowanie, petla do wejscia, ataki z wyslaniem do serwera, okreslenie jednostek 
+}
