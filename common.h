dd2cd26c568df2e5420165a502bd7d317fa34367
Kacper <kacper.noweta@gmail.com>
2026-01-25 23:34:05 +0100

    added includes

--- common.h
+++ common.h
@@ -1,16 +1,18 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-
-
-
-
-
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <math.h>
 
 #define PROJECT_ID 'Z'
 #define MAX_TEXT 512
