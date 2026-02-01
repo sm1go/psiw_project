58b9d87ffc3261569eb1d0e778264b523bf8efd8
Kacper <kacper.noweta@gmail.com>
2026-02-01 17:49:05 +0100

    2

--- client.c
+++ client.c
@@ -4,18 +4,42 @@ int msg_id;
 int my_player_id = -1; 
 int my_msg_type = -1;
 
+void print_line(int line_num, const char* text) {
+    printf("\033[%d;1H", line_num);
+    printf("\033[K");
+    printf("%s", text);
+}
+
 void receiver_loop() {
     Message msg;
     while(1) {
         if (msgrcv(msg_id, &msg, sizeof(Message)-sizeof(long), my_msg_type, 0) == -1) {
             exit(1);
         }
+        
         if (msg.cmd == CMD_UPDATE) {
-            printf("\033[2J\033[H"); 
-            printf("GRACZ %d | %s\n", my_player_id + 1, msg.text);
-            printf("KOMENDY: TRAIN [0-3] [ilosc] | ATTACK [L] [H] [C]\n");
-            printf("(0-Lekka, 1-Ciezka, 2-Jazda, 3-Robotnik)\n> ");
+            printf("\033[s\033[?25l"); 
+
+            char buffer[MAX_TEXT];
+            strcpy(buffer, msg.text);
+            
+            char* line = strtok(buffer, "\n");
+            int row = 2;
+            
+            printf("\033[1;1H\033[KGRACZ %d", my_player_id + 1);
+
+            while(line != NULL) {
+                print_line(row++, line);
+                line = strtok(NULL, "\n");
+            }
+            
+            print_line(row, "------------------------------------------------");
+            print_line(row+1, "KOMENDY: TRAIN [0-3] [ilosc] | ATTACK [L] [H] [C]");
+            print_line(row+2, "(0-Lekka, 1-Ciezka, 2-Jazda, 3-Robotnik)");
+
+            printf("\033[u\033[?25h");
             fflush(stdout);
+
         } else if (msg.cmd == CMD_RESULT) {
             printf("\033[2J\033[H");
             printf("\n\n================================\n");
@@ -47,6 +71,9 @@ int main() {
     my_player_id = msg.args[0];
     my_msg_type = 10 + my_player_id;
 
+    printf("\033[2J");
+    for(int i=0; i<10; i++) printf("\n"); 
+    
     printf("Zalogowano jako Gracz %d\n", my_player_id + 1);
 
     if (fork() == 0) {
@@ -54,25 +81,41 @@ int main() {
     } else {
         char buffer[100];
         while(1) {
-            fgets(buffer, 100, stdin);
+            printf("\033[K> "); 
+            fflush(stdout);
+            
+            if (fgets(buffer, 100, stdin) == NULL) continue;
+            buffer[strcspn(buffer, "\n")] = 0;
+
+            printf("\033[1A\033[K");
+
             char cmd_str[10];
             int a, b, c;
             
-            if (sscanf(buffer, "%s %d %d %d", cmd_str, &a, &b, &c) >= 2) {
+            if (sscanf(buffer, "%s", cmd_str) >= 1) {
                 msg.type = 1; 
                 msg.source_id = getpid();
                 msg.args[0] = my_player_id;
+                int valid = 0;
 
                 if (strcmp(cmd_str, "TRAIN") == 0) {
-                    msg.cmd = CMD_TRAIN;
-                    msg.args[1] = a; 
-                    msg.args[2] = b; 
-                    msgsnd(msg_id, &msg, sizeof(Message)-sizeof(long), 0);
+                    if (sscanf(buffer, "%*s %d %d", &a, &b) == 2) {
+                        msg.cmd = CMD_TRAIN;
+                        msg.args[1] = a; 
+                        msg.args[2] = b; 
+                        valid = 1;
+                    }
                 } else if (strcmp(cmd_str, "ATTACK") == 0) {
-                    msg.cmd = CMD_ATTACK;
-                    msg.args[1] = a; 
-                    msg.args[2] = b; 
-                    msg.args[3] = c; 
+                    if (sscanf(buffer, "%*s %d %d %d", &a, &b, &c) == 3) {
+                        msg.cmd = CMD_ATTACK;
+                        msg.args[1] = a; 
+                        msg.args[2] = b; 
+                        msg.args[3] = c; 
+                        valid = 1;
+                    }
+                }
+
+                if (valid) {
                     msgsnd(msg_id, &msg, sizeof(Message)-sizeof(long), 0);
                 }
             }
